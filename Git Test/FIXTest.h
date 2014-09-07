61963c0f5525de35e3ceb21bb30aa3d1a54d433c
Andrew Bell <fiixed@gmail.com>
2014-09-07 08:42:37 -0500

    added test class

---
+++ Git Test/FIXTest.h
@@ -0,0 +1,13 @@
+//
+//  FIXTest.h
+//  Git Test
+//
+//  Created by Andrew Bell on 9/7/14.
+//  Copyright (c) 2014 Andrew Bell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FIXTest : NSObject
+
+@end
---
+++ Git Test/FIXTest.m
@@ -0,0 +1,13 @@
+//
+//  FIXTest.m
+//  Git Test
+//
+//  Created by Andrew Bell on 9/7/14.
+//  Copyright (c) 2014 Andrew Bell. All rights reserved.
+//
+
+#import "FIXTest.h"
+
+@implementation FIXTest
+
+@end
